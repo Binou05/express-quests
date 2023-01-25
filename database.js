0ecc878e4c4d516d5a3faa5d269c4291a964c682
Binou05 <sabine.arnaud-bertoneri@sncf.fr>
2023-01-25 08:26:51 +0100

    quête 7

--- database.js
+++ database.js
@@ -8,17 +8,8 @@ const database = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  city: process.env.MY_CITY,
-  name: process.env.MY_NAME,
-  language: process.env.MY_LANGUAGE,
+ 
 });
-database
-  .getConnection()
-  .then(() => {
-    console.log("Can reach database");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+
  
     module.exports = database;
